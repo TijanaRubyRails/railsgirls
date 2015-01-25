667bd6551b6792e0ee072d74282971ac0e8e4cc8
rails -v <ls>
2015-01-25 15:20:12 +0100

    poruka

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = 'b0d131ae35b9dfac901946de0663004b284bbe127b7b0b035d94f24480b2e034dacb0824e048d43d21306dedb84b6cb780bf9b5bc6ad0e48ad3ca1573f00baec'
